9229757280fdab90ec996d96c280df8aef1986f0
mateosuster <mateosuster@gmail.com>
2021-11-02 16:14:33 -0300

    cambio archivo

--- src/lightgbm/copia_672_lgb_binaria2.r
+++ src/lightgbm/copia_672_lgb_binaria2.r
@@ -42,10 +42,10 @@ setwd( directory.root )
 kexperimento  <- NA   #NA si se corre la primera vez, un valor concreto si es para continuar procesando
 
 kscript           <- "672_lgb_binaria2"
-# karch_generacion  <- "./datasetsOri/paquete_premium_202009.csv"
-# karch_aplicacion  <- "./datasetsOri/paquete_premium_202011.csv"
-karch_generacion  <- "./datasets/paquete_premium_202009_ext.csv"
-karch_aplicacion  <- "./datasets/paquete_premium_202011_ext.csv"
+karch_generacion  <- "./datasetsOri/paquete_premium_202009.csv"
+karch_aplicacion  <- "./datasetsOri/paquete_premium_202011.csv"
+# karch_generacion  <- "./datasets/paquete_premium_202009_ext.csv"
+# karch_aplicacion  <- "./datasets/paquete_premium_202011_ext.csv"
 kBO_iter    <-  150   #cantidad de iteraciones de la Optimizacion Bayesiana
 
 #Aqui se cargan los hiperparametros
